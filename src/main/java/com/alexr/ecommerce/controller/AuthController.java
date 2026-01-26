a8cdf4661053b0cc0570c73c09f179d230f57fac
AlexRoa6 <alexroasebastian9@gmail.com>
2026-01-26 11:59:15 +0100

    AuthController hecho

--- src/main/java/com/alexr/ecommerce/controller/AuthController.java
+++ src/main/java/com/alexr/ecommerce/controller/AuthController.java
@@ -2,29 +2,36 @@ package com.alexr.ecommerce.controller;
 
 
 import com.alexr.ecommerce.dto.AuthRequestDTO;
-import com.alexr.ecommerce.repository.UsuarioRepository;
-import com.alexr.ecommerce.service.JwtService;
+import com.alexr.ecommerce.dto.AuthResponseDTO;
+import com.alexr.ecommerce.service.UsuarioService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("api/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
-    private UsuarioRepository usuarioRepository;
+    private UsuarioService usuarioService;
 
 
-    public AuthController(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
+    public AuthController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
 
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> registrar(@Valid @RequestBody AuthRequestDTO authRequestDTO){
+        AuthResponseDTO authResponseDTO = usuarioService.registrar(authRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(authResponseDTO);
+    }
 
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login(@Valid @RequestBody AuthRequestDTO authRequestDTO){
+        AuthResponseDTO authResponseDTO = usuarioService.login(authRequestDTO);
+        return ResponseEntity.ok(authResponseDTO);
+    }
 
 
 }
